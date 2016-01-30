c8443de899194d76856621ba4f219e17868fb0f7
Abhimanyu310 <abhip310@gmail.com>
2016-01-29 18:31:40 -0700

    Decided the database and table structure.

---
+++ tournament.sql
@@ -0,0 +1,28 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+
+CREATE table players (
+	id serial primary key,
+	name text NOT NULL
+	);
+
+CREATE table matches (
+	id serial primary key,
+	id1 integer references players(id),
+	id2 integer references players(id)
+	);
+
+CREATE table records(
+	id integer references players(id),
+	played integer NOT NULL,
+	won integer NOT NULL
+	);
